d4fd363f94976a0505e914e8b2919c1021192a80
Josh Sample <joshuawsample@gmail.com>
2018-05-17 22:01:01 -0400

    Redux extension first commit

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export function selectBook(book) {
+//selectBook is an action creator and needs to return an action
+//and an object with a type property
+    return{
+        type: 'BOOK_SELECTED',
+        payload: book
+    }
+    
+}
